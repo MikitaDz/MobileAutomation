5368538725ffeb7c56dcacaecf712cbdd0fcec6f
mikitadz <mikitadzianisevich@gmail.com>
2022-02-15 23:17:09 +0300

    Added full card validation test

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # MobileAutomation
 Mobile app automation project for Salut app
 
-# How to run app
+## How to run app tests on your local device (Linux)
 
-#Set environment variables
+## Before first run
+1. Install Android SDK, Python, Appium, Appium python library, JDK, etc
+2. Set environment variables which pointed to installed Android SDK 
 export ANDROID_HOME=/home/mikitadz/Android/Sdk
 export ANDROID_SDK_ROOT=/home/mikitadz/Android/Sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+## Run
+1. Run Android studio by running `./studio.sh` from Android studio bin folder
+2. Run Appium `appium -a 127.0.0.1` . Port is 4723 by default
+3. From /Sdk/platform-tools run `adb devices` in order to look on the list of devices connected. Device should be connected via usb and Developer mode should be on
+4. (Optional) Run `./uiautomatorviewer` from /Sdk/tools/bin in order to run uiautomatorviewer 
+5. Set inside test config device id from step 3
+6. Run tests from file or from IDE ()
--- tests/testCardInput.py
+++ tests/testCardInput.py
@@ -1,48 +1,87 @@
 from asyncio import wait_for
-from lib2to3.pgen2 import driver
 import unittest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from appium import webdriver
+from driver.driver import Driver
 
-class TestSalutSmokeTestCase(unittest.TestCase):
+class TestSalutCardDataValidation(unittest.TestCase):
 
-    ##TBD  - settings to config
-    def setUp(self): # test method names begin with 'test'
-        desired_capabilities = {}
-        desired_capabilities['platformName'] = 'Android'
-        desired_capabilities['deviceName'] = 'RF8M620Z7VV'
-        desired_capabilities['appPackage'] = 'ru.sberbank.sdakit.companion.prod'
-        desired_capabilities['appActivity'] = 'ru.sberbank.sdakit.companion.main.presentation.screen.CompanionMainActivity'
-        desired_capabilities['unicodeKeyboard'] = True
-        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities)
+    def setUp(self):
+        self.driver = Driver()
 
     def tearDown(self):
-        self.driver.quit()
+        self.driver.instance.quit()
 
     #TBD - Setup separate classes for login, page objects?, DDT
-    def testInvalidCardInputSalut(self):
+    def TestSalutCardDataValidation(self):
+        ## Skipping onboarding
+        skipOnboardingButton = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Пропустить")')
+        skipOnboardingButton.click()
+
         ## Waiting for login button loaded 
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "ru.sberbank.sdakit.companion.prod:id/companion_button_text")))
+        WebDriverWait(self.driver.instance, 10).until(EC.presence_of_element_located((By.ID, "ru.sberbank.sdakit.companion.prod:id/companion_button_text")))
 
-        #Login to the app
-        elementLoginButton = self.driver.find_element_by_id('ru.sberbank.sdakit.companion.prod:id/companion_button_text')
-        elementLoginButton.click()
-
-        ## Waiting for main screen loaded
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "ru.sberbank.sdakit.companion.prod:id/title_card_container")))
-
-        #Press on the gear button
-        #Using XPATH as element is NAF and no better ways to select it. In real world would ask development to fix it 
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//androidx.cardview.widget.CardView[@index='0']//android.widget.LinearLayout[@index='1']")))
-        elementGearButton = self.driver.find_element_by_xpath("//androidx.cardview.widget.CardView[@index='0']/android.widget.LinearLayout[@index='0']/android.widget.LinearLayout[@index='1']/android.widget.LinearLayout[@index='1']")
-        elementGearButton.click()
+        # Login to the app
+        loginButton = self.driver.instance.find_element_by_id('ru.sberbank.sdakit.companion.prod:id/companion_button_text')
+        loginButton.click()
 
         ## Waiting for main screen loaded
-        ##WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By., "Платежи")))ndElementByAndroidUIAutomator("text(\"Text\")")
-        paymentsMenuItem = self.driver.find_element_by_android_uiautomator('new UiSelector().text("Платежи")')
+        WebDriverWait(self.driver.instance, 10).until(EC.presence_of_element_located((By.ID, "ru.sberbank.sdakit.companion.prod:id/title_card_container")))
+
+        # Press on the gear button
+        # Using XPATH as element is NAF and no better ways to select it. In real world would ask development to fix it 
+        WebDriverWait(self.driver.instance, 10).until(EC.presence_of_element_located((By.XPATH, "//androidx.cardview.widget.CardView[@index='0']//android.widget.LinearLayout[@index='1']")))
+        gearButton = self.driver.instance.find_element_by_xpath("//androidx.cardview.widget.CardView[@index='0']/android.widget.LinearLayout[@index='0']/android.widget.LinearLayout[@index='1']/android.widget.LinearLayout[@index='1']")
+        gearButton.click()
+
+        ## Waiting for main screen loaded
+        paymentsMenuItem = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Платежи")')
         paymentsMenuItem.click()
 
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "ru.sberbank.sdakit.companion.prod:id/companion_button_text")))
+        # Wait for card list to be loaded
+        self.driver.instance.implicitly_wait(5)
+
+        addCardButton = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Добавить карту")')
+        addCardButton.click()
+
+        ## Fill in card data
+        # Wait for form to be loaded
+        WebDriverWait(self.driver.instance, 15).until(EC.presence_of_element_located((By.XPATH, "//android.widget.EditText[@resource-id='pan']")))
+
+        # Using XPATH as elements below are NAF and no better ways to select it (despite having id). In real world would ask development to fix it 
+        # input card number
+        cardNumberInput = self.driver.instance.find_element_by_xpath("//android.widget.EditText[@resource-id='pan']")
+        cardNumberInput.send_keys("1234 1234 1234 1234")
+
+        # assert label about invalid card number is displayed. In Requirements - "Неверный код карты" - but I assume functionally is the same
+        invalidCardNumberLabel = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Неверный номер карты")')
+        self.assertTrue(invalidCardNumberLabel.is_displayed())
+
+        # input card date
+        cardNumberDateInput = self.driver.instance.find_element_by_xpath("//android.widget.EditText[@resource-id='expiry']")
+        cardNumberDateInput.send_keys("03/05")
+
+        # assert label about invalid card date is displayed
+        invalidCardDateLabel = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Неверная дата")')
+        self.assertTrue(invalidCardDateLabel.is_displayed())
+
+        # input cvc/cvv
+        cardCVCInput = self.driver.instance.find_element_by_xpath("//android.widget.EditText[@resource-id='cvc']")
+        cardCVCInput.send_keys("123")
+
+        # input email
+        emailInput = self.driver.instance.find_element_by_xpath("//android.widget.EditText[@resource-id='email']")
+        emailInput.send_keys("123@yandex.ru")
+
+        payButton = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Оплатить")')
+
+        # Payments button is by default clickable and enabled, so checking this parameters would make test fail.
+        # I would check if it is expected (as functionally doesn't looks like a bug) and
+        # suggest to check that invalid form label appear after click
+        payButton.click()
+
+        # assert label about invalid card form data is displayed
+        invalidCardDateLabel = self.driver.instance.find_element_by_android_uiautomator('new UiSelector().text("Проверьте правильность заполнения платежной формы")')
+        self.assertTrue(invalidCardDateLabel.is_displayed())
 
